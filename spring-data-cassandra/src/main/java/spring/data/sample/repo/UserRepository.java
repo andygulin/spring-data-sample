ba8157bfe6155039eb37159eae7540d4a9056965
andygulin <andygulin@hotmail.com>
2017-06-28 11:52:53 +0800

    ...

--- spring-data-cassandra/src/main/java/spring/data/sample/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package spring.data.sample.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import spring.data.sample.bean.User;
-
-public interface UserRepository extends CrudRepository<User, String> {
-
-}
